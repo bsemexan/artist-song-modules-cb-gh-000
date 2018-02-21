3294bdfc94568020809965676b002a41c4825be5
Berry  Semexan <bsemexan@gmail.com>
2018-02-21 17:52:38 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
   module ClassMethods
     def reset_all
-      self.clear
+      self.all.clear
     end
 
     def count
-      self.count
+      self.all.count
     end
   end
 end
